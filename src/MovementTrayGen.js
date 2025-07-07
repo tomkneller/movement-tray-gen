fd7872eca1f6198fac5b2a56b9b971e36e727f43
Thomas Kneller <Thomas.kneller@yahoo.com>
2025-07-07 11:40:00 +0100

    Moved tray options to tabs

--- src/MovementTrayGen.js
+++ src/MovementTrayGen.js
@@ -3,6 +3,8 @@ import GridGen from './GridGen';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { STLExporter } from 'three/addons/exporters/STLExporter.js';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 function MovementTrayGenerator() {
     const [circularDiameter, setCircularDiameter] = useState(25);
@@ -180,101 +182,109 @@ function MovementTrayGenerator() {
                 <img src='logo192.png' width={'50'} height={'50'} display={'block'} alt='logo png' ></img>
                 <h2 style={{ paddingLeft: 10 }}> Movement Tray Forge</h2>
             </div>
+
             <div style={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
                 <div style={{ display: 'flex', flex: 'grow', flexDirection: 'column', backgroundColor: '#7b4b1e' }} >
-                    <h3>Base Size</h3>
-                    <div>
-                        <label>Circular Diameter:</label>
-                        <input type="number" name="circularDiameter" value={circularDiameter} onChange={handleInputChange} />
-                    </div>
-                    <p>--------------------------</p>
-
-                    <div inert={hasSupportSlot}>
-                        <div>
-                            <label>Columns:</label>
-                            <input type="number" name="formationCols" value={formationCols} onChange={handleInputChange} />
-                        </div>
-                        <div>
-                            <label>Rows:</label>
-                            <input type="number" name="formationRows" value={formationRows} onChange={handleInputChange} />
-                        </div>
-                    </div>
-                    <p>--------------------------</p>
-                    <h3>Add support slot</h3>
-                    <div>
-                        <label>Support Slot:</label>
-                        <input type="checkbox" name="supportSlot" checked={hasSupportSlot} value={hasSupportSlot} onChange={handleInputChange} />
-                    </div>
-                    <div inert={!hasSupportSlot}>
-                        <div>
-                            <label>Support Mode:</label>
-                            <select name='supportMode' value={supportMode} onChange={handleInputChange}>
-                                <option value={'wrap'}>Wrap</option>
-                                <option value={'ranked'}>Ranked</option>
-                            </select>
-                            Not Yet Implemented
-                        </div>
-                        <div>
-                            <label>Support Slots Count:</label>
-                            <input type="number" name="supportCount" value={supportCount} onChange={handleInputChange} max={maxSlots} />
-                        </div>
-                        <div>
-                            <label>Oval Length:</label>
-                            <input type="number" name="ovalLength" value={ovalLength} onChange={handleInputChange} />
-                        </div>
-                        <div>
-                            <label>Oval Width:</label>
-                            <input type="number" name="ovalWidth" value={ovalWidth} onChange={handleInputChange} />
-                        </div>
-                    </div>
-                    <p>--------------------------</p>
-                    <h3>Add Magnet slots</h3>
-                    <div>
-                        <label>Magnet Slots:</label>
-                        <input type="checkbox" name="magnetSlot" checked={hasMagnetSlot} value={hasMagnetSlot} onChange={handleInputChange} />
-                    </div>
-                    <div inert={!hasMagnetSlot}>
-                        <div>
-                            <label>Magnet Width:</label>
-                            <input type="number" name="magnetWidth" value={magnetWidth} onChange={handleInputChange} min={1} max={circularDiameter - 2} />
-                        </div>
-                        <div>
-                            <label>Magnet Depth:</label>
-                            <input type="number" name="magnetDepth" value={magnetDepth} onChange={handleInputChange} min={1} max={baseThickness - 1} />
-                        </div>
-                    </div>
-                    <p>--------------------------</p>
-                    <div inert={hasSupportSlot}>
-                        <div>
-                            <label>Gap:</label>
-                            <input type="number" name="gap" value={gap} onChange={handleInputChange} />
-                        </div>
-                    </div>
-                    <p>--------------------------</p>
-                    <div>
-                        <label>Base Thickness:</label>
-                        <input type="number" name="baseThickness" value={baseThickness} onChange={handleInputChange} min={2} max={edgeHeight} />
-                    </div>
-                    <div>
-                        <label>Edge Height:</label>
-                        <input type="number" name="edgeHeight" value={edgeHeight} onChange={handleInputChange} min={2} max={10} />
-                    </div>
-                    <div>
-                        <label>Edge Thickness:</label>
-                        <input type="number" name="edgeThickness" value={edgeThickness} onChange={handleInputChange} min={1} />
-                    </div>
-                    <p>--------------------------</p>
-                    <h3>Formation</h3>
-                    <div inert={hasSupportSlot}>
-                        <div>
-                            <label>Stagger Formation:</label>
-                            <input type='checkbox' name="staggerFormation" value={staggerFormation} onChange={handleInputChange} />
-                        </div>
-                        <div inert={!staggerFormation}>
-                            <label>Remove Stray Slots:</label>
-                            <input type="checkbox" name="straySlot" checked={hasStraySlot} value={hasStraySlot} onChange={handleInputChange} />
-                        </div>
-                    </div>
+                    <Tabs>
+                        <TabList>
+                            <Tab>Tray Options</Tab>
+                            <Tab>Support Slots</Tab>
+                            <Tab>Magnet Slots</Tab>
+                            <Tab>Formations</Tab>
+                        </TabList>
+                        <TabPanel>
+                            <h3>Base Size</h3>
+                            <div>
+                                <label>Circular Diameter:</label>
+                                <input type="number" name="circularDiameter" value={circularDiameter} onChange={handleInputChange} />
+                            </div>
+                            <div>
+                                <label>Columns:</label>
+                                <input type="number" name="formationCols" value={formationCols} onChange={handleInputChange} />
+                            </div>
+                            <div>
+                                <label>Rows:</label>
+                                <input type="number" name="formationRows" value={formationRows} onChange={handleInputChange} />
+                            </div>
+                            <div>
+                                <label>Base Thickness:</label>
+                                <input type="number" name="baseThickness" value={baseThickness} onChange={handleInputChange} min={2} max={edgeHeight} />
+                            </div>
+                            <div>
+                                <label>Edge Height:</label>
+                                <input type="number" name="edgeHeight" value={edgeHeight} onChange={handleInputChange} min={2} max={10} />
+                            </div>
+                            <div>
+                                <label>Edge Thickness:</label>
+                                <input type="number" name="edgeThickness" value={edgeThickness} onChange={handleInputChange} min={1} />
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <h3>Add support slot</h3>
+                            <div>
+                                <label>Support Slot:</label>
+                                <input type="checkbox" name="supportSlot" checked={hasSupportSlot} value={hasSupportSlot} onChange={handleInputChange} />
+                            </div>
+                            <div inert={!hasSupportSlot}>
+                                <div>
+                                    <label>Support Mode:</label>
+                                    <select name='supportMode' value={supportMode} onChange={handleInputChange}>
+                                        <option value={'wrap'}>Wrap</option>
+                                        <option value={'ranked'}>Ranked</option>
+                                    </select>
+                                    Not Yet Implemented
+                                </div>
+                                <div>
+                                    <label>Support Slots Count:</label>
+                                    <input type="number" name="supportCount" value={supportCount} onChange={handleInputChange} max={maxSlots} />
+                                </div>
+                                <div>
+                                    <label>Oval Length:</label>
+                                    <input type="number" name="ovalLength" value={ovalLength} onChange={handleInputChange} />
+                                </div>
+                                <div>
+                                    <label>Oval Width:</label>
+                                    <input type="number" name="ovalWidth" value={ovalWidth} onChange={handleInputChange} />
+                                </div>
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <h3>Add Magnet slots</h3>
+                            <div>
+                                <label>Magnet Slots:</label>
+                                <input type="checkbox" name="magnetSlot" checked={hasMagnetSlot} value={hasMagnetSlot} onChange={handleInputChange} />
+                            </div>
+                            <div inert={!hasMagnetSlot}>
+                                <div>
+                                    <label>Magnet Width:</label>
+                                    <input type="number" name="magnetWidth" value={magnetWidth} onChange={handleInputChange} min={1} max={circularDiameter - 2} />
+                                </div>
+                                <div>
+                                    <label>Magnet Depth:</label>
+                                    <input type="number" name="magnetDepth" value={magnetDepth} onChange={handleInputChange} min={1} max={baseThickness - 1} />
+                                </div>
+                            </div>
+                            <div inert={hasSupportSlot}>
+                                <div>
+                                    <label>Gap:</label>
+                                    <input type="number" name="gap" value={gap} onChange={handleInputChange} />
+                                </div>
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <h3>Formation</h3>
+                            <div inert={hasSupportSlot}>
+                                <div>
+                                    <label>Stagger Formation:</label>
+                                    <input type='checkbox' name="staggerFormation" value={staggerFormation} onChange={handleInputChange} />
+                                </div>
+                                <div inert={!staggerFormation}>
+                                    <label>Remove Stray Slots:</label>
+                                    <input type="checkbox" name="straySlot" checked={hasStraySlot} value={hasStraySlot} onChange={handleInputChange} />
+                                </div>
+                            </div>
+                        </TabPanel>
+                    </Tabs>
                     <div>
                         <button onClick={generateVisualization}>Visualize</button>
                         <button onClick={handleDownloadSTL}>Download STL</button>
